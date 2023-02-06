352ca38cfe3739dfd8857b1896d236505c2afe78
james yates <yates_7@hotmail.com>
2023-02-06 15:51:53 +0000

    Spring boot initial setup commit

---
+++ Vrem_Spring/src/main/java/com/VREM/Vrem/VremApplication.java
@@ -0,0 +1,13 @@
+package com.VREM.Vrem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VremApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VremApplication.class, args);
+	}
+
+}
