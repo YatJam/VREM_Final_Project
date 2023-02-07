1878e023f4857c2176f9b196ee5cbbb56db4ab72
james yates <yates_7@hotmail.com>
2023-02-07 15:28:17 +0000

    weapon controller created

---
+++ Vrem_Spring/src/main/java/controllers/WeaponController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import models.Weapon;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import repositories.WeaponRepository;
+
+import java.util.List;
+
+@RestController
+public class WeaponController {
+
+    @Autowired
+    WeaponRepository weaponRepository;
+
+    @GetMapping(value = "/weapons")
+    public ResponseEntity<List<Weapon>> getAllWeapons(){
+        return  new ResponseEntity<>(weaponRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/weapons/{id}")
+    public ResponseEntity getWeapon(@PathVariable Long id){
+        return new ResponseEntity<>(weaponRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/weapons")
+    public ResponseEntity<Weapon> postWeapon(@RequestBody Weapon weapon){
+        weaponRepository.save(weapon);
+        return new ResponseEntity<>(weapon, HttpStatus.CREATED);
+    }
+
+}
