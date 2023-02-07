de34ecec120ed383ceb1631df916f96a396d1537
james yates <yates_7@hotmail.com>
2023-02-07 17:30:41 +0000

    test data added

--- Vrem_Spring/src/main/java/com/VREM/Vrem/components/DataLoader.java
+++ Vrem_Spring/src/main/java/com/VREM/Vrem/components/DataLoader.java
@@ -1,5 +1,9 @@
 package com.VREM.Vrem.components;
 
+import com.VREM.Vrem.models.Armour;
+import com.VREM.Vrem.models.CharacterClass;
+import com.VREM.Vrem.models.User;
+import com.VREM.Vrem.models.Weapon;
 import com.VREM.Vrem.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -30,6 +34,16 @@ public class DataLoader implements ApplicationRunner{
     }
 
     public void run(ApplicationArguments args) {
-        
+        CharacterClass Dwarf = new CharacterClass("Dwarf", 400, "I am going to chin you");
+        characterClassRepository.save(Dwarf);
+
+        Weapon barehands = new Weapon("Bare Hands", 5);
+        weaponRepository.save(barehands);
+
+        Armour rags = new Armour("Rags", 1);
+        armourRepository.save(rags);
+
+        User John = new User("John", Dwarf, rags, barehands);
+        userRepository.save(John);
     }
 }
