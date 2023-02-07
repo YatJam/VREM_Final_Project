5092fe0ae3e41d7061de0434534b4e7e5e809861
james yates <yates_7@hotmail.com>
2023-02-07 16:25:01 +0000

    armour test passed in Spring

--- Vrem_Spring/src/test/java/com/VREM/Vrem/VremApplicationTests.java
+++ Vrem_Spring/src/test/java/com/VREM/Vrem/VremApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import repositories.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
