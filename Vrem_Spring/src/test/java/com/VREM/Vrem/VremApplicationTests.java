3fb223c8d7e956a488d3496969f92351d424a32d
james yates <yates_7@hotmail.com>
2023-02-07 17:02:09 +0000

    item and user creation tests implimented and passed

--- Vrem_Spring/src/test/java/com/VREM/Vrem/VremApplicationTests.java
+++ Vrem_Spring/src/test/java/com/VREM/Vrem/VremApplicationTests.java
@@ -1,6 +1,6 @@
 package com.VREM.Vrem;
 
-import com.VREM.Vrem.models.Armour;
+import com.VREM.Vrem.models.*;
 import com.VREM.Vrem.repositories.*;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -38,6 +38,34 @@ class VremApplicationTests {
 		Armour leatherArmour = new Armour("Leather Armour", 10);
 		armourRepository.save(leatherArmour);
 	}
+
+	@Test
+	public void canAddWeaponItemCharacterClassToDB(){
+		Weapon sword = new Weapon("Sword", 42);
+		weaponRepository.save(sword);
+
+		Item key = new Item("Key", 1);
+		itemRepository.save(key);
+
+		CharacterClass Dwarf = new CharacterClass("Dwarf", 120, "I am going to stab you");
+		characterClassRepository.save(Dwarf);
+	}
+
+	@Test
+	public void canCreateStarterCharacter(){
+		CharacterClass Dwarf = new CharacterClass("Dwarf", 400, "I am going to chin you");
+		characterClassRepository.save(Dwarf);
+
+		Weapon barehands = new Weapon("Bare Hands", 5);
+		weaponRepository.save(barehands);
+
+		Armour rags = new Armour("Rags", 1);
+		armourRepository.save(rags);
+
+		User John = new User("John", Dwarf, rags, barehands);
+		userRepository.save(John);
+	}
+
 
 
 
