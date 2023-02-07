c9c7efd728f66f0e161fd79fcd29f505633ef0fe
james yates <yates_7@hotmail.com>
2023-02-07 15:48:26 +0000

    spring is running and connected to database, no errors

--- Vrem_Spring/src/test/java/com/VREM/Vrem/VremApplicationTests.java
+++ Vrem_Spring/src/test/java/com/VREM/Vrem/VremApplicationTests.java
@@ -1,6 +1,7 @@
 package com.VREM.Vrem;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -30,5 +31,7 @@ class VremApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
