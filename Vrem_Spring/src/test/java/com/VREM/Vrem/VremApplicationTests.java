8ecc1b698522c8de097337463688164b0a2f3caf
james yates <yates_7@hotmail.com>
2023-02-07 15:40:22 +0000

    testing area built

--- Vrem_Spring/src/test/java/com/VREM/Vrem/VremApplicationTests.java
+++ Vrem_Spring/src/test/java/com/VREM/Vrem/VremApplicationTests.java
@@ -1,10 +1,31 @@
 package com.VREM.Vrem;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import repositories.*;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class VremApplicationTests {
+
+	@Autowired
+	ArmourRepository armourRepository;
+
+	@Autowired
+	CharacterClassRepository characterClassRepository;
+
+	@Autowired
+	ItemRepository itemRepository;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	WeaponRepository weaponRepository;
 
 	@Test
 	void contextLoads() {
