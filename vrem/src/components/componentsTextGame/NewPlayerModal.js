45fd2d00c9c11c881f8785786c61c866d54c5aef
james yates <yates_7@hotmail.com>
2023-02-09 23:32:25 +0000

    form building for create player

--- vrem/src/components/componentsTextGame/NewPlayerModal.js
+++ vrem/src/components/componentsTextGame/NewPlayerModal.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './NewPlayerModal.css'
 
 function NewPlayerModal({ closeModal }) {
+    const [character, setCharacter] = useState("Amazon")
+
+  const onOptionChange = e => {
+    setCharacter(e.target.value)
+  }
 
   return ReactDom.createPortal(
     <div className="modalBackground">
@@ -16,6 +22,72 @@ function NewPlayerModal({ closeModal }) {
             </div>
             <div className="body">
                 <p>Insert your player details.</p>
+                <form className="createPlayerForm">
+                    <div>
+                    <label htmlFor='name'>Player Name</label>
+                    <input type='text' name="playerName" placeholder='Player Name' />
+                    </div>
+                    <div>
+                    <input
+                        type="radio"
+                        name="character"
+                        value="Amazon"
+                        id="Amazon"
+                        checked={character === "Amazon"}
+                        onChange={onOptionChange}
+                    />
+                    <label htmlFor="amazon">Amazon</label>
+
+                    <input
+                        type="radio"
+                        name="character"
+                        value="Dwarf"
+                        id="Dwarf"
+                        checked={character === "Dwarf"}
+                        onChange={onOptionChange}
+                    />
+                    <label htmlFor="dwarf">Dwarf</label>
+
+                    <input
+                        type="radio"
+                        name="character"
+                        value="Outcast"
+                        id="Outcast"
+                        checked={character === "Outcast"}
+                        onChange={onOptionChange}
+                    />
+                    <label htmlFor="outcast">Outcast</label>
+
+                    <input
+                        type="radio"
+                        name="character"
+                        value="Paladin"
+                        id="Paladin"
+                        checked={character === "Paladin"}
+                        onChange={onOptionChange}
+                    />
+                    <label htmlFor="paladin">Paladin</label>
+
+                    <input
+                        type="radio"
+                        name="character"
+                        value="Warlock"
+                        id="Warlock"
+                        checked={character === "Warlock"}
+                        onChange={onOptionChange}
+                    />
+                    <label htmlFor="warlock">Warlock</label>
+
+                    <p>
+                        Select character <strong>{character}</strong>
+                    </p>
+                    </div>
+                    <div>
+                        <label htmlFor='terms'>Terms of service</label>
+                        <input type="checkbox" name="terms" value="checked" />
+                    </div>
+
+                </form>
             </div>
             <div className="footer">
                 <button onClick={() => closeModal(false)} id="cancelBtn">Cancel</button>
