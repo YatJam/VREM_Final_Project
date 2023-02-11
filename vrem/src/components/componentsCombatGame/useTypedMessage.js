053b9b3f9d0b9bb05d27036d65d0266e1886a530
james yates <yates_7@hotmail.com>
2023-02-11 00:09:09 +0000

    typed message built

--- vrem/src/components/componentsCombatGame/useTypedMessage.js
+++ vrem/src/components/componentsCombatGame/useTypedMessage.js
@@ -6,8 +6,20 @@ const useTypedMessage = (message) => {
     const [typedMessage, setTypedMessage] = useState(" ");
 
 useEffect(() => {
-    
-})
+    setTypedMessage(' ');
+    if (message.length){
+        (async ()=> {
+            let visibleMessage = '';
+
+            for(let i = 0; i < message.length; i++){
+                await wait(25);
+                visibleMessage = visibleMessage + message[i];
+                setTypedMessage(visibleMessage);
+            }
+        })();
+    }
+
+}, [message]);
 
   return typedMessage;
     
